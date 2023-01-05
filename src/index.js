75683fa19777118a5bbfdd6e6232898b89dc3a08
Nelly Akrabi <akrabinelly@mail.ru>
2023-01-05 15:44:46 +0100

    added Marvel API with API-kay

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hallo");
+
+import axios from "axios"
+
+const API_KEY = 'a5837db97d72016c81a7a776f4240db9';
+const url = "https://gateway.marvel.com/v1/public/stories"
+
+axios.get(url, {
+    params: {
+        apikey: API_KEY
+    }
+})
+    .then(result => console.log(result.data))
+    .catch(error => console.log(error.massage))
