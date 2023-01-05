d04b364b072ff3942070c2ead035f1a02a365256
Nelly Akrabi <akrabinelly@mail.ru>
2023-01-05 18:53:44 +0100

    minimal style

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,12 @@
 
 import { url, urlComics } from "../../constants/api";
-import { getDataApi } from "../../utils/getDataApi"
+import { getDataApi } from "../../utils/getDataApi";
+import "./App.css"
 
 class App {
-
     async render() {
         const data = await getDataApi.getData(url + urlComics);
         console.log("result", data);
-
     }
 }
 
